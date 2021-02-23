9c72cd1c9f9a6d395879a2954c572987b565a384
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-23 15:01:40 -0500

    commit tree 5786640ff2b85e52a022948fe2cf95a71b6c98da

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(shoe_name)
+        @brand = shoe_name
+    end
+def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+end
+end
